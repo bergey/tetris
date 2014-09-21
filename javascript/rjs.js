c3e09a07d442c65ad9293c26654d91a26b32b647
Daniel Bergey <bergey@alum.mit.edu>
2014-09-21 20:33:48 +0000

    js: initial commit: build tools template

---
+++ javascript/rjs.js
@@ -0,0 +1,13 @@
+require.config({
+  shim: {
+
+  },
+  paths: {
+    react: "bower_components/react/react",
+    requirejs: "bower_components/requirejs/require",
+    underscore: "bower_components/underscore/underscore"
+  },
+  packages: [
+
+  ]
+});
